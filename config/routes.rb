5d8975813ea5df7fdb4590335e600f87faadf42b
Alles Rebel <allesrebel@yahoo.com>
2014-01-28 15:20:56 -0800

    finished up routes and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'  #same as get "static_pages/help
   match '/about',   to: 'static_pages#about',   via: 'get'  #same as get "static_pages/about
   match '/contact', to: 'static_pages#contact', via: 'get'  #same as get "static_pages/contact
+  match '/signup', to: 'users#new', via: 'get'
 end
