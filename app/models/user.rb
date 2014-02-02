e1b88b1021724596308040d6464270d40cd822fe
Alles Rebel <allesrebel@yahoo.com>
2014-02-01 21:54:29 -0800

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   
   #mask for email stuff (checks if syntax is right)
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   #Checks if the field is null, checks against the format, and checks if there is only one
   validates :email, presence:   true, 
