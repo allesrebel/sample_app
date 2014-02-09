2d372e6f7b73655d7a354d7c5bbf1b55e9150ec9
Alles Rebel <allesrebel@yahoo.com>
2014-02-09 01:40:30 -0800

    Added Sign in and out system with cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,36 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase};
+  before_create :create_remember_token
 
   #checks if the field is null, and checks if the length is less than 50
   validates :name, presence: true, length: {maximum: 50}
-  
+
   #mask for email stuff (checks if syntax is right)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   #Checks if the field is null, checks against the format, and checks if there is only one
-  validates :email, presence:   true, 
-                    format:     {with: VALID_EMAIL_REGEX}, 
-                    uniqueness: {case_senstive: false}
+  validates :email, presence:   true,
+  format:     {with: VALID_EMAIL_REGEX},
+  uniqueness: {case_senstive: false}
   #auto checks for presence of password and much more
   has_secure_password
 
   #check if the password is there and has a minimum length
   validates :password, length: { minimum: 6}
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  root  'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'  #same as get "static_pages/help
-  match '/about',   to: 'static_pages#about',   via: 'get'  #same as get "static_pages/about
-  match '/contact', to: 'static_pages#contact', via: 'get'  #same as get "static_pages/contact
-  match '/signup',  to: 'users#new',            via: 'get'
-  
   resources :users
+  resources :sessions, only: [:new, :create, :destory]
+
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'    #same as get "static_pages/help
+  match '/about',   to: 'static_pages#about',   via: 'get'    #same as get "static_pages/about
+  match '/contact', to: 'static_pages#contact', via: 'get'    #same as get "static_pages/contact
+  match '/signup',  to: 'users#new',            via: 'get'    
+  match '/signout', to: 'sessions#destroy',     via: 'delete' 
+  match '/signin',  to: 'sessions#new',         via: 'get'
 end
